d641b0175e0996d1440cbb383f4e7be627469a99
Viktoriia Bryzhak <viaylya@gmail.com>
2024-01-26 20:16:35 +0200

    HW 4. Розгалудження та цикли

---
+++ conditions_cycles/task3_for_while.js
@@ -0,0 +1,14 @@
+const numberFour = 4;
+let multiplyResult;
+
+for (let i = 1; i < 11; i++) {
+  multiplyResult = numberFour * i;
+  console.log(`${numberFour} x ${i} = ${multiplyResult}`);
+}
+
+let i = 1;
+while (i < 11) {
+  multiplyResult = numberFour * i;
+  console.log(`${numberFour} x ${i} = ${multiplyResult}`);
+  i++;
+}
